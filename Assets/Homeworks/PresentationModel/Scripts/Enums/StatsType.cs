64ccb014e7b569a113e55f73d41f9cf5d78da897
Teleshko Tatiana <dev.tatiana@vivexgames.com>
2026-02-19 23:21:40 +0300

    Update enums

---
+++ Assets/Homeworks/PresentationModel/Scripts/Enums/StatsType.cs
@@ -0,0 +1,19 @@
+using System;
+using UnityEngine;
+
+namespace Common.Enums
+{
+    public enum StatsType 
+    {
+        [LocName("Move Speed")]
+        MoveSpeed,
+        Stamina,
+        Dexterity,
+        Intelligence,
+        Damage,
+        Regeneration
+    
+    }
+
+}
+
